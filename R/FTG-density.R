f4c0651d049287a45f63a69c2ea3ff245db88e7d
SergiVilardell <sergi.vilardell13@gmail.com>
2019-01-11 11:11:25 +0100

    remove threshold

--- R/FTG-density.R
+++ R/FTG-density.R
@@ -1,9 +1,8 @@
 #' Density function
 #'
 #' This function computes the density of the full-tail gamma with the input sample data. The expression for the density used is:
-#' \deqn{f(x; \alpha, \theta, \rho, u) = \theta(\rho + \theta(x + u))^{\alpha - 1}\exp(-(\rho + \theta(u + x)))/ \Gamma(\alpha, \rho + \theta u)}
+#' \deqn{f(x; \alpha, \theta, \rho) = \theta(\rho + \theta x)^{\alpha - 1}\exp(-(\rho + \theta x))/ \Gamma(\alpha, \rho)}
 #' @param x Sample data.
-#' @param threshold Minimum value of the tail.
 #' @param alpha alpha.
 #' @param theta theta.
 #' @param rho rho.
@@ -15,13 +14,10 @@
 #' r <- 0.8
 #' n <- 1000
 #' sample <- rFTG(n, a, t, r)
-#' sample <- sort(sample)
-#' u <- sample[length(sample) - n/2]
-#' xu <- seq(u, max(sample), length.out = 200)
-#' du <- dFTG(xu, a, t, r, u)
+#' x <- seq(min(sample), max(sample), length.out = 200)
+#' d <- dFTG(x, a, t, r)
 #' hist(sample, breaks = "FD", probability = TRUE)
-#' lines(xu, du, col = "red")
-dFTG <- function(x, alpha, theta, rho, threshold) {
-  theta * (rho + theta * threshold + theta * x)^(alpha - 1) *
-    exp(-(rho + theta * threshold + theta * x)) / gsl::gamma_inc(alpha, rho + theta * threshold)
+#' lines(x, d, col = "red")
+dFTG <- function(x, alpha, theta, rho) {
+  theta * (rho + theta * x)^(alpha - 1) * exp(-(rho + theta * x)) / gsl::gamma_inc(alpha, rho)
 }
--- R/FTG-probability.R
+++ R/FTG-probability.R
@@ -1,9 +1,9 @@
 #' Probability function
 #'
 #' This function computes the probability of the full-tail gamma with the input sample data. The expression for the density used is:
-#' \deqn{F(x; \alpha, \theta, \rho, u) = 1- \frac{\Gamma(\alpha, \rho + \theta(x + u))}{\Gamma(\alpha, \rho + \theta u)}}
+#' \deqn{F(x; \alpha, \theta, \rho) = 1- \frac{\Gamma(\alpha, \rho + \theta x)}{\Gamma(\alpha, \rho)}}
 #' @param x Sample data.
-#' @param threshold Minimum value of the tail.
+
 #' @param alpha alpha.
 #' @param theta theta.
 #' @param rho rho.
@@ -11,6 +11,6 @@
 #' @export
 #' @examples
 #' #pFTG()
-pFTG <- function(x, alpha, theta, rho, threshold){
-  1- gsl::gamma_inc(alpha, rho + theta * (x + threshold))/gsl::gamma_inc(alpha, rho + theta * threshold)
+pFTG <- function(x, alpha, theta, rho){
+  1- gsl::gamma_inc(alpha, rho + theta*x)/gsl::gamma_inc(alpha, rho)
 }
