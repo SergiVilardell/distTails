727af63b568aeb2c633466d30133bbdbc27d82da
SergiVilardell <sergi.vilardell13@gmail.com>
2019-05-23 12:34:35 +0200

    Typo

--- R/FTG-loglikelihood.R
+++ R/FTG-loglikelihood.R
@@ -1,8 +1,8 @@
 #' Loglikelihood function
 #'
 #' This function computes the loglikelihood of the full-tail gamma with the input sample data. The expression used is:
-#' \deqn{l(x; \alpha, \sigma, \rho) = -n(\log\Gamma(\lpha, \rho) + \log(\sigma\rho^{-\alpha}) - \frac{\alpha - 1}{n}
-#' \sum_{i = 1}{n}\log(1 + \frac{x_{i}}{\sigma}) + \frac{\rho}{n} \sum_{i = 1}{n}(1 + \frac{x_{i}}{\sigma}))}
+#' \deqn{l(x; \alpha, \sigma, \rho) = -n(\log\Gamma(\alpha, \rho) + \log(\sigma\rho^{-\alpha}) - \frac{\alpha - 1}{n}
+#' \sum_{i = 1}{n}\log(1 + \frac{x_{i}}{\sigma}) + \frac{\rho}{n} \sum_{i = 1}{n}(1 + \frac{x_{i}}{\sigma})
 #' @param x Sample data.
 #' @param alpha alpha.
 #' @param theta theta.
